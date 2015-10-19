394899e9c2c9e07297eff02e1b027d9aab4ccdd7
Michal Suder <michalsuder@wp.pl>
2015-10-19 16:21:08 +0200

    Working lab 2

--- src/main/scala/pl/suder/scala/AuctionHouse.scala
+++ src/main/scala/pl/suder/scala/AuctionHouse.scala
@@ -3,8 +3,10 @@ package pl.suder.scala
 import akka.actor._
 import akka.event.LoggingReceive
 import scala.concurrent.duration.`package`.DurationInt
+import scala.util.Random
+import pl.suder.scala._
 
-object Auction {
+object AuctionMessage {
   case class Bid(bid: Int) {
     require(bid > 0)
   }
@@ -12,18 +14,18 @@ object Auction {
   case object EndAuction
   case object DeleteAuction
   
-  val BidTimer = 5 second
-  val DeleteTimer = 2 second
-}
-
-object Person {
-  case object Sold
-  case object Buyed
+  val BidTimer = 10 seconds
+  val DeleteTimer = 5 seconds
+  
+  case object ItemSold
+  case object ItemBuyed
+  case object NotEnough
+  case object Beaten
 }
 
 class Auction extends Actor {
-  import Auction._ 
-  import scala.concurrent.ExecutionContext.Implicits.global
+  import AuctionMessage._ 
+  import context._
   
   def Created: Receive = LoggingReceive {
     case Bid(bid) => 
@@ -42,51 +44,58 @@ class Auction extends Actor {
   }
   
   def Activated(prize: Int, buyer: ActorRef): Receive = LoggingReceive {
-    case Bid(bid) if bid > prize => 
+    case Bid(bid) if bid > prize =>
+      buyer ! Beaten
       context become Activated(bid, sender)
+    case Bid(_) => sender ! NotEnough
     case EndAuction =>
-      buyer ! Person.Buyed
+      buyer ! ItemBuyed
       // TODO notify Seller
       startDeleteTimer()
       context become Sold
   }
   
   def Sold: Receive = LoggingReceive {
-    case DeleteAuction =>
-      context stop self
+    case Bid(_) => sender ! ItemSold
+    case DeleteAuction => context stop self
   }
   
   def startEndTimer() {
-    println("Start end timer")
-    context.system.scheduler.scheduleOnce(BidTimer, self, EndAuction)
+    system.scheduler.scheduleOnce(BidTimer, self, EndAuction)
   }
   
   def startDeleteTimer() {
-    context.system.scheduler.scheduleOnce(DeleteTimer, self, DeleteAuction)
+    system.scheduler.scheduleOnce(DeleteTimer, self, DeleteAuction)
   }
   
-  def receive = Created
   startEndTimer()
+  def receive = Created
 }
 
 class Buyer(val auctions: List[ActorRef]) extends Actor {
-  import Auction._
+  import AuctionMessage._
+  import context._
   
-  def receive: Receive = LoggingReceive {
-    case _ => ???
+  def bid(auction: ActorRef, bid: Int) = system.scheduler.scheduleOnce(Random.nextInt(2000) + 100 milliseconds, auction, Bid(bid))
+
+  def buying(sold: Int): Receive = LoggingReceive {
+    case ItemSold | ItemBuyed if sold + 1 == auctions.size => context stop self 
+    case ItemSold | ItemBuyed => context become buying(sold + 1)
+    case NotEnough => bid(sender, 10)
+    case Beaten => bid(sender, 10)
   }
   
-  while(!auctions.isEmpty) {
-    Thread sleep 1000
-    auctions.foreach { auction => auction ! Bid(10)}
-  }
+  def receive = buying(0)
+  auctions.foreach { auction => bid(auction, 10) }
 }
 
 object AuctionHouse extends App {
   val system = ActorSystem("AuctionHouse")
-  val auctions = List();
-  val action = system.actorOf(Props[Auction], "auction")
-  val buyer = system.actorOf(Props(classOf[Buyer], List(action)), "buyer")
-
+  
+  var auctions: List[ActorRef] = List()
+  0 to 10 foreach {i => auctions = (system.actorOf(Props[Auction], "auction" + i) :: auctions)}
+//  0 to 10 foreach {i => auctions = (system.actorOf(Props[AuctionFSM], "auction" + i) :: auctions)}
+  
+  0 to 5 foreach {i => system.actorOf(Props(classOf[Buyer], auctions), "buyer" + i)}
   system.awaitTermination()
 }
